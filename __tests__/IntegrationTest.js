ff78d529e78efdb99a7c152ea1b2a187b3f6027f
inseong01 <insung940@naver.com>
2025-10-18 11:24:48 +0900

    test: 계산기 통합 테스트 케이스 추가
    - 성공/예외 테스트 케이스 추가
    - 커스텀 구분자 '\' 일 때 테스트 실패 확인

--- __tests__/IntegrationTest.js
+++ __tests__/IntegrationTest.js
@@ -21,34 +21,57 @@ const correctCase = [
   { inputs: ['0:1:1'], output: '결과 : 2' },
   { inputs: [''], output: '결과 : 0' },
   { inputs: ['//q\\n1q2q3'], output: '결과 : 6' },
+  { inputs: [' '], output: '결과 : 0' },
+  { inputs: ['123'], output: '결과 : 123' },
+  { inputs: ['+1,2'], output: '결과 : 3' },
+  { inputs: ['2,+2'], output: '결과 : 4' },
+  { inputs: ['1, 2:1 :3 ,3'], output: '결과 : 10' },
+  { inputs: ['//0x\\n10x20x3'], output: '결과 : 6' },
+  { inputs: ['//\\\\n1\\2\\3'], output: '결과 : 6' },
 ]
 
 const exceptedCase = [
   { inputs: ['0x12,2,2'], output: '[ERROR]' },
+  { inputs: ['0b12,2,2'], output: '[ERROR]' },
+  { inputs: ['0,2!2'], output: '[ERROR]' },
+  { inputs: ['0:1,'], output: '[ERROR]' },
+  { inputs: ['0q1'], output: '[ERROR]' },
+  { inputs: ['1,-1,0'], output: '[ERROR]' },
+  { inputs: ['1,-1,--0'], output: '[ERROR]' },
+  { inputs: ['1,1,*3'], output: '[ERROR]' },
+  { inputs: ['\\n,1'], output: '[ERROR]' },
+  { inputs: ['\\'], output: '[ERROR]' },
+  { inputs: ['//\\n123'], output: '[ERROR]' },
+  { inputs: ['//1\\n11213'], output: '[ERROR]' },
+  { inputs: ['//0x2\\n10x220x23'], output: '[ERROR]' },
 ]
 
 describe("문자열 계산기 통합", () => {
-  test.each(correctCase)('성공 - 입력: $inputs $output ', async ({ inputs, output }) => {
-    mockQuestions(inputs);
+  describe('성공', () => {
+    test.each(correctCase)('입력: $inputs → $output"', async ({ inputs, output }) => {
+      mockQuestions(inputs);
 
-    const logSpy = getLogSpy();
+      const logSpy = getLogSpy();
 
-    const app = new App();
-    await app.calculator();
+      const app = new App();
+      await app.calculator();
 
-    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    })
   })
 
-  test.each(exceptedCase)('예외 - 입력: $inputs $output ', async ({ inputs, output }) => {
-    mockQuestions(inputs);
+  describe('예외', () => {
+    test.each(exceptedCase)('입력: $inputs → $output', async ({ inputs, output }) => {
+      mockQuestions(inputs);
 
-    const logSpy = getLogSpy();
+      const logSpy = getLogSpy();
 
-    async function calculator() {
-      const app = new App();
-      return await app.calculator()
-    }
-    await expect(calculator).rejects.toThrow('[ERROR]');
-    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+      async function calculator() {
+        const app = new App();
+        return await app.calculator()
+      }
+      await expect(calculator).rejects.toThrow('[ERROR]');
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    })
   })
 });
